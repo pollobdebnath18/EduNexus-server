c3a07a19753b9b78112bbe05d82bdab8e39c4576
pollob debnath <pullobdebnath@gmail.com>
2026-08-28 13:26:06 +0600

    fix postgres railway config

--- config/database.js
+++ config/database.js
@@ -1,20 +1,15 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      connectionString: env('DATABASE_URL'),
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false) && {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+      connectionString: env("DATABASE_URL"),
+      ssl: {
+        rejectUnauthorized: false,
       },
     },
     pool: {
-      min: env.int('DATABASE_POOL_MIN', 2),
-      max: env.int('DATABASE_POOL_MAX', 10),
+      min: env.int("DATABASE_POOL_MIN", 2),
+      max: env.int("DATABASE_POOL_MAX", 10),
     },
   },
 });
